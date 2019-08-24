35c2d1122af13e24ff35a2f5f3401e78d10b0559
Zafar Hussain <zhuss@nets.eu>
2019-08-24 13:17:41 +0530

    Hello @Dev

--- app/src/main/java/ness/com/etw/authenticate/view/HomeActivity.java
+++ app/src/main/java/ness/com/etw/authenticate/view/HomeActivity.java
@@ -31,6 +31,7 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
     private BottomNavigationView bnvFooter;
 
 // Deve branch created
+    // Hello
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
